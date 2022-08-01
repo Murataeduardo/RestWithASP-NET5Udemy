192e70111dfd25d702d85c8bba0585200270b423
unknown <murata.eduardo@gmail.com>
2022-08-01 17:54:19 -0300

    Corrigindo a implementação a classe e interface Person

---
+++ 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASP-NETUdemy/RestWithASP-NETUdemy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASP_NETUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASP_NETUdemy.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
