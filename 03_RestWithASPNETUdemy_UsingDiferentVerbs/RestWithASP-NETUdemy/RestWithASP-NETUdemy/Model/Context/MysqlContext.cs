ab593b4fd00a0d856cb0187f4fd8a4f3a36c376d
unknown <murata.eduardo@gmail.com>
2022-08-01 20:08:25 -0300

    Definindo as propriedades de conexao com a base de dados

---
+++ 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASP-NETUdemy/RestWithASP-NETUdemy/Model/Context/MysqlContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestWithASP_NETUdemy.Model.Context
+{
+    public class MysqlContext : DbContext 
+    {
+        public MysqlContext() 
+        {
+        }
+        public MysqlContext(DbContextOptions<MysqlContext> options) : base(options){}
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
